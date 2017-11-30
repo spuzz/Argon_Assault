42ada4ede0430a4c07db9e07d29a467d9ebaf432
ASPURRELL <acspuzz@gmail.com>
2017-11-30 13:51:18 +0000

    Lecture 83

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [Tooltip("in ms^-1")][SerializeField] float xSpeed = 4f;
+    float xOffset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xOffset = (xThrow * xSpeed) * Time.deltaTime;
+        print(Time.deltaTime);
+	}
+}
