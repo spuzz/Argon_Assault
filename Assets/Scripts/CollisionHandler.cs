5dd435fe4fd4ead6d3cf88940af0654717c59f9e
ASPURRELL <acspuzz@gmail.com>
2017-12-07 13:53:55 +0000

    Lecture 95

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour {
+
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")] [SerializeField] GameObject deathFX;
+    private void OnTriggerEnter(Collider other)
+    {
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        deathFX.SetActive(true);
+        SendMessage("OnPlayerDeath");
+        Invoke("RestartLevel", levelLoadDelay);
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
