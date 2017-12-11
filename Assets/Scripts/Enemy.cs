062fbe1a0b12304092f6b6f4dd7e0b00cf699e1c
ASPURRELL <acspuzz@gmail.com>
2017-12-11 13:53:10 +0000

    lecture 102 - ScoreBoard changes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    [SerializeField] int scorePerHit = 10;
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+
+    ScoreBoard scoreBoard;
+
 	// Use this for initialization
 	void Start () {
         AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     private void AddNonTriggerBoxCollider()
@@ -20,6 +25,7 @@ public class Enemy : MonoBehaviour {
 
     private void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit(scorePerHit);
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -6,19 +6,22 @@ using UnityEngine.UI;
 public class ScoreBoard : MonoBehaviour {
 
 
-    [SerializeField] int scorePerHit = 12;
+    
+    [SerializeField] int scorePerSecond = 1;
     int score;
     Text scoreText;
 
+    float timer;
 	// Use this for initialization
 	void Start () {
+        timer = 0;
         scoreText = GetComponent<Text>();
         scoreText.text = score.ToString();
 
     }
 	
 
-    public void ScoreHit()
+    public void ScoreHit(int scorePerHit)
     {
         score = score + scorePerHit;
         scoreText.text = score.ToString();
@@ -26,6 +29,12 @@ public class ScoreBoard : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
+        timer += Time.deltaTime;
+        if(timer > 1)
+        {
+            score += scorePerSecond;
+            scoreText.text = score.ToString();
+            timer -= 1;
+        }
 	}
 }
