758d6e83614070cac6f750496b3693d5696e4b1d
ASPURRELL <acspuzz@gmail.com>
2017-12-11 12:39:57 +0000

    Lecture 98

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AddNonTriggerBoxCollider();
+        
+
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider  collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
