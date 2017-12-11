e42d39f42718b919ff84e95683749924726cbe78
ASPURRELL <acspuzz@gmail.com>
2017-12-11 13:23:13 +0000

    lecture 101 - ScoreBoard

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
 	// Use this for initialization
 	void Start () {
         AddNonTriggerBoxCollider();
-        
-
     }
 
     private void AddNonTriggerBoxCollider()
@@ -20,6 +20,8 @@ public class Enemy : MonoBehaviour {
 
     private void OnParticleCollision(GameObject other)
     {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
     // Update is called once per frame
